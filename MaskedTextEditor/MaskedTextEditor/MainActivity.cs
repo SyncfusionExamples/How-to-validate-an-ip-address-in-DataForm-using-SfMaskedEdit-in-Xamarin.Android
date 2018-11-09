dc8be8db2473deb33c54cadabad9dcc94e66a073
ROOT\swathi.gopal <swathi.gopal@syncfusion.com>
2018-11-09 11:54:43 +0530

    Included sample project for SfMasked edit sample in Android.

---
+++ MaskedTextEditor/MaskedTextEditor/MainActivity.cs
@@ -0,0 +1,140 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Syncfusion.Android.DataForm;
+using Syncfusion.Android.DataForm.Editors;
+using Syncfusion.Android.MaskedEdit;
+using Android.Content;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Android.Graphics;
+
+namespace MaskedTextEditor
+{
+    [Activity(Label = "MaskedTextEditor", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        SfDataForm dataForm;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            dataForm = new SfDataForm(this);
+            dataForm.DataObject = new ExpenseInfo();
+            dataForm.SetBackgroundColor(Color.White);
+            dataForm.RegisterEditor("CustomMaskEditors", new CustomMaskEditor(dataForm,this));
+            dataForm.RegisterEditor("Balance", "CustomMaskEditors");
+            // Set our view from the "main" layout resource
+            SetContentView(dataForm);
+        }
+    }
+
+    public class CustomMaskEditor : DataFormEditor<SfMaskedEdit>
+    {
+        Context context;
+        public CustomMaskEditor(SfDataForm dataForm,Context _context) : base(dataForm)
+        {
+            context = _context;
+        }
+
+        protected override SfMaskedEdit OnCreateEditorView()
+        {
+            return new SfMaskedEdit(context);
+        }
+        protected override void OnInitializeView(DataFormItem dataFormItem, SfMaskedEdit view)
+        {
+            view.Mask = @"000\.000\.000\.000";
+            view.Value =(string)this.DataForm.ItemManager.GetValue(dataFormItem);
+        }
+        protected override void OnWireEvents(SfMaskedEdit view)
+        {
+            view.ValueChanged += View_ValueChanged;
+        }
+
+        void View_ValueChanged(object sender, ValueChangedEventArgs eventArgs)
+        {
+            OnCommitValue(sender as SfMaskedEdit);
+            OnValidateValue(sender as SfMaskedEdit);
+        }
+
+        protected override void OnUnWireEvents(SfMaskedEdit view)
+        {
+            view.ValueChanged -= View_ValueChanged;
+        }
+        protected override bool OnValidateValue(SfMaskedEdit view)
+        {
+           return this.DataForm.Validate("Balance");
+        }
+        protected override void OnCommitValue(SfMaskedEdit view)
+        {
+            var dataFormItemView = view.Parent as DataFormItemView;
+            this.DataForm.ItemManager.SetValue(dataFormItemView.DataFormItem, view.Value);
+        }
+    }
+
+    public class ExpenseInfo : INotifyPropertyChanged, INotifyDataErrorInfo
+    {
+
+        public void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        private string balance = "100";
+        public string Balance
+        {
+            get
+            {
+                return balance;
+            }
+            set
+            {
+                balance = value;
+                RaisePropertyChanged("Balance");
+            }
+        }
+        private string _ItemName = "Education";
+        public string ItemName
+        {
+            get
+            {
+                return _ItemName;
+            }
+            set
+            {
+                _ItemName = value;
+            }
+        }
+
+        [Display(AutoGenerateField = false)]
+        public bool HasErrors
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            var list = new List<string>();
+            if (!propertyName.Equals("Balance"))
+                return list;
+            else
+            {
+                // Here, you can validate Mask Editor. 
+            }
+            return list;
+        }
+
+    }
+
+}
+
